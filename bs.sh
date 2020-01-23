4c22db6f4de5d3b2770e42ed48e9a35d303c9347
viz <visuweshmuthukumar@gmail.com>
2020-01-23 18:28:01 +0530

    bs.sh: save two bytes by using ternary

--- bs.sh
+++ bs.sh
@@ -1,1 +1,1 @@
-while((n!=i));do((h<l))&&{ m=-1;break;};((m=(l+h)/2));n=${a[$m]};((i>n?l++:i<n?h--:1));done
+while((n!=i));do((l>h?m=-1:0))&&break;((m=(l+h)/2));n=${a[$m]};((i>n?l++:i<n?h--:0));done
