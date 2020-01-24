ea5c86d9c3cd213ba2f547f2da66e0b402e62207
viz <visuweshmuthukumar@gmail.com>
2020-01-24 19:49:42 +0530

    foldl.sh: use A instead of _a

--- foldl.sh
+++ foldl.sh
@@ -1,1 +1,1 @@
-f(){ p=$1;i=$2;nameref _=$3;_a=();shift 2;j=-1;while((j++,j<${#_[*]}));do for u { nameref _=$u;_a+=(${_[$j]});};i=`$p $i ${_a[*]}`;_a=();done;echo $i;}
+f(){ p=$1;i=$2;nameref _=$3;A=();shift 2;j=-1;while((j++,j<${#_[*]}));do for u { nameref _=$u;A+=(${_[$j]});};i=`$p $i ${A[*]}`;A=();done;echo $i;}
