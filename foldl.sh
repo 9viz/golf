a4437d9980984e72246f23b3ed94551bff041fe4
viz <visuweshmuthukumar@gmail.com>
2020-01-26 12:25:25 +0530

    foldl.sh: pre-increment j to save some bytes

--- foldl.sh
+++ foldl.sh
@@ -1,1 +1,1 @@
-f(){ p=$1;i=$2;nameref _=$3;A=();shift 2;j=-1;while((j++,j<${#_[*]}));do for u { nameref _=$u;A+=(${_[$j]});};i=`$p $i ${A[*]}`;A=();done;echo $i;}
+f(){ p=$1;i=$2;nameref _=$3;A=();shift 2;j=-1;while((++j<${#_[*]}));do for u { nameref _=$u;A+=(${_[$j]});};i=`$p $i ${A[*]}`;A=();done;echo $i;}
